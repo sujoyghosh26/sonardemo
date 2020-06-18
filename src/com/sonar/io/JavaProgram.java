3d0f2cc3c3cf7c9514a622dee9b8cae5612e9126
Sujoy Ghosh <51069811+sujoyghosh26@users.noreply.github.com>
2020-06-17 22:35:14 -0700

    Add files via upload

---
+++ src/com/sonar/io/JavaProgram.java
@@ -0,0 +1,38 @@
+package com.sonar.io;
+
+public class JavaProgram {
+	public static void main(String args[]){
+		int a[]={1,2,3,2,11,15,14,16};
+		System.out.println("Odd Numbers:");
+		for(int i=0;i<a.length;i++){
+			if(a[i]%2!=0){
+				System.out.println(a[i]);
+			}
+		}
+		System.out.println("Even Numbers:");
+		for(int i=0;i<a.length;i++){
+			if(a[i]%2==0){
+				System.out.println(a[i]);
+			}
+		}
+		
+		
+		String input = "GeeksforGeeks"; 
+		  
+        // getBytes() method to convert string  
+        // into bytes[]. 
+        byte [] strAsByteArray = input.getBytes(); 
+  
+        byte [] result =  
+                   new byte [strAsByteArray.length]; 
+  
+        // Store result in reverse order into the 
+        // result byte[] 
+        for (int i = 0; i<strAsByteArray.length; i++) 
+            result[i] =  
+             strAsByteArray[strAsByteArray.length-i-1]; 
+  
+        System.out.println(new String(result)); 
+	}
+	
+}
