2fc87d9abded56a1fd777753cd1fe62157b66726
sujoyghosh26 <sghosh@kovair.com>
2020-06-18 00:03:05 -0700

    a

--- src/com/sonar/io/JavaProgram.java
+++ src/com/sonar/io/JavaProgram.java
@@ -10,6 +10,7 @@ public class JavaProgram {
 			}
 		}
 		System.out.println("Even Number:");
+		System.out.println("Even Number:")
 		for(int i=0;i<a.length;i++){
 			if(a[i]%2==0){
 				System.out.println(a[i]);
